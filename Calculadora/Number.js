766a0b43ad431f3303746238b24586fc0052ad3e
Lucas de Sousa Alves <83097518+LucaSousAlves@users.noreply.github.com>
2023-11-22 23:17:45 -0300

    Add files via upload

---
+++ Calculadora/Number.js
@@ -0,0 +1,11 @@
+function somaNumeros(numero) {
+    let soma = 0;
+  
+    for (let i = 1; i < numero; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            soma += i;
+        }
+    }
+  
+    return soma;
+}
